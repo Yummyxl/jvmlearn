25523b34ca5a9518fd9c34f13ba60ac59688837c
Yummgxl <2548341581@qq.com>
2020-02-14 19:50:57 +0800

    准备阶段的意义

--- src/main/java/section1/classloader/Test6.java
+++ src/main/java/section1/classloader/Test6.java
@@ -19,15 +19,14 @@ public class Test6 {
 class Singleton {
 
     public static int a;
-//    public static Singleton singleton = new Singleton();
-    public static int b = 0;
+//    public static int b = 0;
     public static Singleton singleton = new Singleton();
 
     private Singleton() {
         a++;
-        b++;
+        b++; //准备阶段的意义 如果没准备阶段赋初值的话，在这里b是没有值的
     }
-
+    public static int b = 0;
     public static Singleton getinstance() {
         return singleton;
     }
