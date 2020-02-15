18a35c6941349e706b093373479fef2d4465ae59
Yummgxl <2548341581@qq.com>
2020-02-15 11:23:10 +0800

    接口和类初始化异同及双亲委托机制

---
+++ src/main/java/section1/classloader/Test7.java
@@ -0,0 +1,35 @@
+package section1.classloader;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ *
+ * @Author: Yummyxl
+ * @Date: 2020/2/15
+ *
+ * 初始化一个类不会初始化它实现的接口
+ * 可以把Parent7修改成class验证父类会被初始化
+ * 也可以把Child7修改成接口来验证接口初始化会不会带来父接口的初始化
+ * 都可以通过查看反编译代码来验证
+ */
+
+public class Test7 {
+
+    public static void main(String[] args) {
+        System.out.println(Child7.b);
+    }
+}
+
+interface Parent7 {
+    public static final Thread t = new Thread() {
+        {
+            // 如果有输出，说明t被赋予了实例值，即Parent7类被初始化了
+            System.out.println("parent7 invoke");
+
+        }
+    };
+}
+
+class Child7 implements Parent7 {
+    public static int b = 7;
+}
